18ece758fc37a42a467e0a9e563d34d10b3cbec3
zhicong <zhicong@sunlands.com.sg>
2025-01-13 15:39:48 +0800

    create and add a NotFound page

--- mobile-web-shop/src/main.jsx
+++ mobile-web-shop/src/main.jsx
@@ -5,11 +5,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CartPage from './pages/CartPage';
 import PurchasePage from './pages/PurchasePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />
+    element: <HomePage />,
+    errorElement: <NotFoundPage />
   },
   {
     path: '/cart',
