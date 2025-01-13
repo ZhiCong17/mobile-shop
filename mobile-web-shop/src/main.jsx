512a35853d8a16e06c61f423f785a9df532277bd
zhicong <zhicong@sunlands.com.sg>
2025-01-13 15:27:19 +0800

    create routing to home, cart and purchase page

--- mobile-web-shop/src/main.jsx
+++ mobile-web-shop/src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Purchase from './pages/Purchase';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '/purchase',
+    element: <Purchase />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
