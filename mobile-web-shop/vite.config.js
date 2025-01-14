492d2ab3be01ca708ac6207ca3ba6be3d45e9695
zhicong <zhicong@sunlands.com.sg>
2025-01-14 18:46:19 +0800

    install and set up vite plugin mock

--- mobile-web-shop/vite.config.js
+++ mobile-web-shop/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from "path";
+import { viteMockServe } from 'vite-plugin-mock';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteMockServe({
+      mockPath: 'mock',
+      localEnabled: true,
+      prodEnabled: false,
+      supportTs: false,
+      watchFiles: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
