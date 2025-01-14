83e9db6fb9ebd45db45c470ce05f685f01d971aa
zhicong <zhicong@sunlands.com.sg>
2025-01-14 10:43:05 +0800

    create a NavBar component

---
+++ mobile-web-shop/src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import { ShoppingCart, LayoutGrid, ScrollText } from 'lucide-react';
+
+function NavBar() {
+  return (
+    <nav className='fixed bottom-0 mt-4 bg-white w-full py-2'>
+      <ul className='flex'>
+        <li className='w-1/3' >
+          <Link className='flex flex-col items-center' to="/"><LayoutGrid size={32}/>Home</Link>
+        </li>
+        <li className='w-1/3' >
+          <Link className='flex flex-col items-center' to="/cart"><ShoppingCart size={32}/>Cart</Link>
+        </li>
+        <li className='w-1/3' >
+          <Link className='flex flex-col items-center' to="/purchase"><ScrollText size={32}/>Purchase</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
