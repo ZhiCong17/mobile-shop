57eeff03ac1b0f51743d767b339c24cb198aa3f3
zhicong <zhicong@sunlands.com.sg>
2025-01-14 16:56:46 +0800

    create login page and login route

--- mobile-web-shop/src/pages/login/LoginForm.jsx
+++ mobile-web-shop/src/pages/login/LoginForm.jsx
@@ -12,9 +12,9 @@ function LoginForm() {
 
   return (
     <form action=''>
-      <p className='mb-1'>Email</p>
+      <p className='font-bold mb-1'>Email</p>
       <input className='mb-5 pl-3 h-10 border border-neutral-500 rounded w-full' type='email' value={email} name='email' autoComplete='email' placeholder='me@example.com' onChange={e => setEmail(e.target.value)} />
-      <p className='mb-1'>Password</p>
+      <p className='font-bold mb-1'>Password</p>
       <input className='mb-5 pl-3 h-10 border border-neutral-500 rounded w-full' type='password' value={password} name='password' placeholder='********' onChange={e => setPassword(e.target.value)} />
       <Button className='w-full' onClick={handleSubmit}>Login</Button>
     </form>
