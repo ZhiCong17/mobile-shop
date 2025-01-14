5c2ae20f66c5b9c05e025c0343b873b028ced930
zhicong <zhicong@sunlands.com.sg>
2025-01-14 16:56:46 +0800

    create login page and login route

---
+++ mobile-web-shop/src/pages/login/LoginPage.jsx
@@ -0,0 +1,20 @@
+import LoginForm from './LoginForm';
+import { Link } from 'react-router-dom';
+
+function LoginPage() {
+  return (
+    <div className>
+      <Link className='text-blue-500 block mx-5 mt-5' to='/'>Back to Home</Link>
+      <div className='flex justify-center items-center min-h-[calc(100vh-84px)]'>
+        <div>
+          <h1 className='text-center font-bold text-2xl mb-1'>Login</h1>
+          <p className='text-center mb-5'>Enter your email below to login to your account</p>
+          <LoginForm />
+          <p className='my-5 text-center'>Dont have an account? <Link className='underline underline-offset-4' to='/cart'>Sign up</Link></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
