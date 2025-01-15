c6d5d242415187f7019486199ac8d1e503e251d8
zhicong <zhicong@sunlands.com.sg>
2025-01-15 17:04:19 +0800

    shift login logic to zustand store and add user state hydration

--- mobile-web-shop/src/store.js
+++ mobile-web-shop/src/store.js
@@ -1,6 +1,14 @@
 import { create } from 'zustand';
 
-export const useUserStore = create((set) => ({
-  user: null,
-  login: (user) => set({ user }),
-}))
+export const useUserStore = create((set) => {
+  const storedUser = localStorage.getItem('user');
+  const loggedInUser = storedUser ? JSON.parse(storedUser) : null;
+
+  return {
+    user: loggedInUser,
+    login: (user) => {
+      set({ user });
+      localStorage.setItem('user', JSON.stringify(user));
+    },
+  }
+})
