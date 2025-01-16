b47e646be442e2ca1716fef9391da54ddad33e3d
zhicong <zhicong@sunlands.com.sg>
2025-01-16 16:06:23 +0800

    shift validate user login function to a file

--- mobile-web-shop/mock/user.js
+++ mobile-web-shop/mock/user.js
@@ -1,4 +1,4 @@
-import users from './data/users.json' assert { type: 'json' };
+import validateUser from "./validateUser";
 
 export default [
   {
@@ -27,20 +27,3 @@ export default [
     }
   }
 ]
-
-function validateUser(loginInfo) {
-  const user = users.find(user => user.email === loginInfo.email);
-
-  if (user && user.password === loginInfo.password) {
-    return {
-      status: 200,
-      message: 'Login successful',
-      user: { id: user.id, email: user.email }
-    }
-  } else {
-    return {
-      status: 401,
-      message: 'Invalid email or password'
-    }
-  }
-}
