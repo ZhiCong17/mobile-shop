5184a5fe9fd752fab3f4a43d900d5b159f64738b
zhicong <zhicong@sunlands.com.sg>
2025-01-17 16:21:52 +0800

    add z index to navbar and modify padding and width of product card

--- mobile-web-shop/src/components/NavBar.jsx
+++ mobile-web-shop/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { ShoppingCart, LayoutGrid, ScrollText } from 'lucide-react';
 
 function NavBar() {
   return (
-    <nav className='fixed bottom-0 mt-4 bg-white w-full py-2 shadow-[0_-4px_6px_0_rgba(0,0,0,0.05)]'>
+    <nav className='fixed bottom-0 mt-4 z-50 bg-white w-full py-2 shadow-[0_-4px_6px_0_rgba(0,0,0,0.05)]'>
       <ul className='flex'>
         <li className='w-1/3' >
           <Link className='flex flex-col items-center' to="/"><LayoutGrid size={32}/>Home</Link>
--- mobile-web-shop/src/pages/home/ProductCard.jsx
+++ mobile-web-shop/src/pages/home/ProductCard.jsx
@@ -5,7 +5,7 @@ const product = products[0];
 
 function ProductCard() {
   return (
-    <div className='flex p-4 gap-2 w-9/12'>
+    <div className='flex pl-4 pb-4 pr-0 gap-2'>
       <img src={product.image} alt={product.name} className='w-24 h-24 rounded' />
       <div className='relative w-full'>
         <p className='mt-2'>{product.name}</p>
