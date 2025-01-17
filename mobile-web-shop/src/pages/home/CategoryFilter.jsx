fbc57cb2d8950ea2ca026c6d722611316c6a1766
zhicong <zhicong@sunlands.com.sg>
2025-01-17 17:41:28 +0800

    create a category filter menu component

---
+++ mobile-web-shop/src/pages/home/CategoryFilter.jsx
@@ -0,0 +1,13 @@
+function CategoryFilterMenu() {
+  return (
+    <div className='bg-neutral-200 h-full rounded-r-lg'>
+      <p className='px-2 py-4 text-sm border-b border-b-neutral-500'>example1</p>
+      <p className='px-2 py-4 text-sm border-b border-b-neutral-500'>example2</p>
+      <p className='px-2 py-4 text-sm border-b border-b-neutral-500 break-words'>example3432432423</p>
+      <p className='px-2 py-4 text-sm border-b border-b-neutral-500'>example4</p>
+      <p className='px-2 py-4 text-sm'>example5</p>
+    </div>
+  )
+}
+
+export default CategoryFilterMenu;
