b17b0b3b744259c8ccc42032e39cb89dc651b01b
zhicong <zhicong@sunlands.com.sg>
2025-01-20 16:38:46 +0800

    use category filter menu to set global category state and filter product display

--- mobile-web-shop/src/pages/home/CategoryFilterMenu.jsx
+++ mobile-web-shop/src/pages/home/CategoryFilterMenu.jsx
@@ -1,17 +1,35 @@
 import products from '../../../mock/data/products.json' with { type: 'json' };
+import { useCategoryStore } from '../../store';
 
 function CategoryFilterMenu() {
   const categories = products.map((product) => product.category);
   const uniqueCategories = [...new Set(categories)];
   const isLastCategory = (category) => category === uniqueCategories[uniqueCategories.length - 1];
+
+  // Global category state
+  const categoryInStore = useCategoryStore(state => state.category);
+  const setCategory = useCategoryStore(state => state.setCategory);
+  const handleCategoryClick = e => {
+    if (e.target.innerText === categoryInStore) {
+      setCategory('');
+      return;
+    }
+
+    setCategory(e.target.innerText)
+  };
+
   const categoryListDisplay = uniqueCategories.map((category) => (
-    <li key={category} className={`px-2 py-3 text-sm border-b break-words ${isLastCategory(category) ? '' : 'border-b-neutral-500'}`}>
+    <li
+      key={category}
+      className={`px-2 py-3 text-sm border-b break-words ${isLastCategory(category) ? '' : 'border-b-neutral-500'} ${category === categoryInStore ? 'text-blue-500' : ''}`}
+      onClick={handleCategoryClick}
+    >
       {category}
     </li>
   ));
 
   return (
-    <ul className='bg-neutral-200 h-full rounded-r-lg'>
+    <ul className='bg-neutral-200 h-full min-h-[calc(100vh-160px)] rounded-r-lg'>
       {categoryListDisplay}
     </ul>
   )
--- mobile-web-shop/src/pages/home/ProductDisplay.jsx
+++ mobile-web-shop/src/pages/home/ProductDisplay.jsx
@@ -1,10 +1,13 @@
 import products from '../../../mock/data/products.json' with { type: 'json' };
 import ProductCard from './ProductCard';
-import { useSearchStore } from '../../store';
+import { useSearchStore, useCategoryStore } from '../../store';
 
 function ProductDisplay() {
   const search = useSearchStore(state => state.search);
-  const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search));
+  const category = useCategoryStore(state => state.category);
+  const filteredProducts = products.filter(product => {
+    return product.name.toLowerCase().includes(search) && product.category.includes(category);
+  });
 
   const display = filteredProducts.map((product) => {
     return <ProductCard key={product.id} product={product} />
