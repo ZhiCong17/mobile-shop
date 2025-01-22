111c20aea02a2d4541b3c15fbf75a96816f837e6
zhicong <zhicong@sunlands.com.sg>
2025-01-22 16:31:08 +0800

    modify className of elements in product card

--- mobile-web-shop/src/pages/home/ProductCard.jsx
+++ mobile-web-shop/src/pages/home/ProductCard.jsx
@@ -3,8 +3,8 @@ import ProductDrawer from './ProductDrawer';
 function ProductCard({ product }) {
   return (
     <div className='flex pl-4 pb-4 pr-0 gap-2'>
-      <img src={product.image} alt={product.name} className='w-24 h-24 rounded' />
-      <div className='relative w-full'>
+      <img src={product.image} alt={product.name} className='w-24 h-24 rounded object-cover' />
+      <div className='relative w-32'>
         <p className='mt-2'>{product.name}</p>
         <div className='absolute bottom-2 left-0 flex justify-between w-full'>
           <p className='my-auto'>${product.price}</p>
