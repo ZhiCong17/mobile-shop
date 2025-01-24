641effb3a18f7dd9b475016381370e9b746cc7e7
zhicong <zhicong@sunlands.com.sg>
2025-01-24 11:22:14 +0800

    edit router to remove navbar from all routes

--- mobile-web-shop/src/main.jsx
+++ mobile-web-shop/src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, Outlet, RouterProvider, useLocation } from 'react-router-dom';
+import { StrictMode } from 'react';
+import './index.css';
+import ReactDOMClient from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 // Import Pages
 import HomePage from './pages/home/HomePage';
@@ -11,54 +11,35 @@ import NotFoundPage from './pages/NotFoundPage';
 import LoginPage from './pages/login/LoginPage';
 import SignUpPage from './pages/sign-up/SignUpPage';
 
-// Import Components
-import NavBar from './components/NavBar';
-
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <NavBarWrapper />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />
-      },
-      {
-        path: '/cart',
-        element: <CartPage />
-      },
-      {
-        path: '/order',
-        element: <OrderPage />
-      },
-      {
-        path: '/login',
-        element: <LoginPage />
-      },
-      {
-        path: '/signup',
-        element: <SignUpPage />
-      }
-    ]
+    element: <HomePage />
   },
-
+  {
+    path: '/cart',
+    element: <CartPage />
+  },
+  {
+    path: '/order',
+    element: <OrderPage />
+  },
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/signup',
+    element: <SignUpPage />
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />
+  }
 ])
 
-function NavBarWrapper() {
-  const location = useLocation();
-  const isLoginPage = location.pathname === '/login';
-  const isSignUpPage = location.pathname === '/signup';
-
-  return (
-    <div>
-      {!isLoginPage && !isSignUpPage && <NavBar />}
-      <Outlet />
-    </div>
-  )
-}
-
-createRoot(document.getElementById('root')).render(
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
