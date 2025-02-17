600afae5d48a17ca67afa502d34f75b5eaeb593d
zhicong <zhicong@sunlands.com.sg>
2025-02-17 16:48:46 +0800

    create a zustand product store

---
+++ mobile-web-shop/src/store/useProductStore.js
@@ -0,0 +1,27 @@
+import { create } from 'zustand';
+import supabase from '@/utils/supabase';
+
+const useProductStore = create((set) => ({
+  products: [],
+  loading: false,
+  hasFetched: false,
+
+  fetchProducts: async () => {
+    set({ loading: true });
+
+    try {
+      const { data: products, error } = await supabase
+        .from('product')
+        .select('*');
+
+      if (error) throw error;
+
+      set({ products, loading: false, hasFetched: true });
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      set({ loading: false });
+    }
+  }
+}))
+
+export default useProductStore;
