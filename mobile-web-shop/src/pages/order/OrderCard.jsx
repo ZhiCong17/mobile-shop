35b59486eb8ee4855b5794a152a3283fc8a0226c
zhicong <zhicong@sunlands.com.sg>
2025-03-24 17:51:45 +0800

    update order card component to display updated order prop

--- mobile-web-shop/src/pages/order/OrderCard.jsx
+++ mobile-web-shop/src/pages/order/OrderCard.jsx
@@ -1,43 +1,48 @@
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent } from "@/components/ui/card";
 
 const OrderCard = ({ order }) => {
-  const totalAmount = order.items.reduce((total, item) => {
-    return total + item.price * item.quantity;
-  }, 0);
+  let totalAmountCents = 0;
 
-  const itemsDisplay = order.items.map((item) => {
-    const { productId, quantity, name, price } = item
+  const itemsDisplay = order.order_item.map((item) => {
+    const {
+      product: { name, price },
+      quantity,
+    } = item;
+
+    totalAmountCents += Math.round(price * 100) * quantity;
 
     return (
-      <div key={productId} className="flex justify-between mb-3">
+      <div key={name} className="flex justify-between mb-3">
         <div>
-          <h3 className='font-bold'>{name}</h3>
+          <h3 className="font-bold">{name}</h3>
           <p>Quantity: {quantity}</p>
         </div>
         <div>
-          <p className='font-bold'>${price * quantity}</p>
+          <p className="font-bold">
+            ${Math.round(price * 100 * quantity) / 100}
+          </p>
         </div>
       </div>
-    )
-  })
+    );
+  });
 
   return (
-    <Card className='mb-5'>
-      <CardContent className='p-5'>
-        <div className='flex justify-between'>
-          <h2 className='font-bold'>Order ID: #{order.id}</h2>
+    <Card className="mb-5">
+      <CardContent className="p-5">
+        <div className="flex justify-between">
+          <h2 className="font-bold">Order ID: #{order.id}</h2>
           <p>Order Status: {order.status}</p>
         </div>
-        <hr className='my-1'/>
-        { itemsDisplay }
-        <hr className='my-1'/>
-        <div className='flex justify-between font-bold'>
+        <hr className="my-1" />
+        {itemsDisplay}
+        <hr className="my-1" />
+        <div className="flex justify-between font-bold">
           <p>Total</p>
-          <p>${totalAmount.toFixed(2)}</p>
+          <p>${totalAmountCents / 100}</p>
         </div>
       </CardContent>
     </Card>
   );
-}
+};
 
 export default OrderCard;
