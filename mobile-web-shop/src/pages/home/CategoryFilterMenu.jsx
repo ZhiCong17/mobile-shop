4e02a593d18df4d62baa25f6adca53bc4c7b1d78
zhicong <zhicong@sunlands.com.sg>
2025-03-25 18:44:17 +0800

    implement fetch categories in CategoryFilterMenu component and update component with new data

--- mobile-web-shop/src/pages/home/CategoryFilterMenu.jsx
+++ mobile-web-shop/src/pages/home/CategoryFilterMenu.jsx
@@ -1,17 +1,31 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
+import { useEffect } from "react";
+
 import useProductStore from "@/store/useProductStore";
 
 function CategoryFilterMenu() {
-  const { products, loading, categoryFilter, setCategoryFilter } =
-    useProductStore();
+  const {
+    loading,
+    categoryFilter,
+    setCategoryFilter,
+    categories,
+    hasFetchedCategories,
+    fetchCategories,
+  } = useProductStore();
 
-  const categories = products.map((product) => product.category);
-  const uniqueCategories = [...new Set(categories)];
-  const isLastCategory = (category) =>
-    category === uniqueCategories[uniqueCategories.length - 1];
-  const isFirstCategory = (category) => category === uniqueCategories[0];
+  // Fetch product categories on component mount if not already fetched
+  useEffect(() => {
+    if (!hasFetchedCategories) {
+      fetchCategories();
+    }
+  }, []);
 
+  // Determine if a category is the last or first
+  const isLastCategory = (index) => index === categories.length - 1;
+  const isFirstCategory = (index) => index === 0;
+
+  // Set or reset category filter state on clicking a category
   const handleCategoryFilterClick = (e) => {
     if (e.target.innerText === categoryFilter) {
       setCategoryFilter("");
@@ -20,29 +34,29 @@ function CategoryFilterMenu() {
     }
   };
 
-  const categoryListDisplay = uniqueCategories.map((category) => (
-    <li
-      key={category}
-      className={`
-        px-2 py-3 text-sm border-b break-words
-        ${!isLastCategory(category) && "border-b-neutral-500"}
-        ${category === categoryFilter && "text-white bg-zinc-400"}
-        ${
-          category === categoryFilter &&
-          isLastCategory(category) &&
-          "rounded-br-lg"
-        }
-        ${
-          category === categoryFilter &&
-          isFirstCategory(category) &&
-          "rounded-tr-lg"
-        }`}
-      onClick={handleCategoryFilterClick}
-    >
-      {category}
-    </li>
-  ));
+  // Display categories
+  const categoryListDisplay = categories.map((category, index) => {
+    const { id, name } = category;
 
+    return (
+      <li
+        key={id}
+        className={`
+        px-2 py-3 text-sm border-b break-words
+        ${!isLastCategory(index) && "border-b-neutral-500"}
+        ${name === categoryFilter && "text-white bg-zinc-400"}
+        ${name === categoryFilter && isLastCategory(index) && "rounded-br-lg"}
+        ${
+          name === categoryFilter && isFirstCategory(index) && "rounded-tr-lg"
+        }`}
+        onClick={handleCategoryFilterClick}
+      >
+        {name}
+      </li>
+    );
+  });
+
+  // Display loading skeleton
   if (loading) {
     return <Skeleton className="h-lvh rounded-r-lg sm:w-[160px]" />;
   }
