e12d90b265d24336692308441245193ed7fa4ab3
zhicong <zhicong@sunlands.com.sg>
2025-03-25 18:38:37 +0800

    create a function to fetch and set categories from database in product store

--- mobile-web-shop/src/store/useProductStore.js
+++ mobile-web-shop/src/store/useProductStore.js
@@ -1,5 +1,5 @@
-import { create } from 'zustand';
-import supabase from '@/utils/supabase';
+import { create } from "zustand";
+import supabase from "@/utils/supabase";
 
 const useProductStore = create((set) => ({
   products: [],
@@ -11,24 +11,44 @@ const useProductStore = create((set) => ({
 
     try {
       const { data: products, error } = await supabase
-        .from('product')
-        .select('*');
+        .from("product")
+        .select("*");
 
       if (error) throw error;
 
       set({ products, loading: false, hasFetched: true });
     } catch (error) {
-      console.error('Error fetching products:', error);
+      console.error("Error fetching products:", error);
       set({ loading: false });
     }
   },
 
-  categoryFilter: '',
+  categoryFilter: "",
   setCategoryFilter: (categoryFilter) => set({ categoryFilter }),
-  clearCategoryFilter: () => set({ categoryFilter: '' }),
+  clearCategoryFilter: () => set({ categoryFilter: "" }),
 
-  searchInput: '',
+  searchInput: "",
   setSearchInput: (searchInput) => set({ searchInput }),
-}))
+
+  // Fetching Product Categories
+  categories: [],
+  hasFetchedCategories: false,
+  fetchCategories: async () => {
+    const url = "http://localhost:3000/api/category";
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error("Response status", response.status);
+      }
+
+      const data = await response.json();
+      const categories = data.categories;
+      set({ categories, hasFetchedCategories: true });
+    } catch (error) {
+      console.error("Error fetching product categories:", error);
+    }
+  },
+}));
 
 export default useProductStore;
