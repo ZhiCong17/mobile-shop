647d7d29c9e19d07485cb2f968c9f3eae98dda24
zhicong <zhicong@sunlands.com.sg>
2025-03-25 14:19:47 +0800

    update handle log out to use function from new user store

--- mobile-web-shop/src/pages/order/LogoutButton.jsx
+++ mobile-web-shop/src/pages/order/LogoutButton.jsx
@@ -1,45 +1,54 @@
-import { Button } from '@/components/ui/button';
-import { useToast } from '@/components/hooks/use-toast';
-import { ToastAction } from '@/components/ui/toast';
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/hooks/use-toast";
+import { ToastAction } from "@/components/ui/toast";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-import { useUserStore } from '@/store';
+import useUserStore from "@/store/useUserStore";
 
-function LogoutButton(props) {
-  const { className } = props;
+function LogoutButton({ className }) {
   const { toast } = useToast();
-  const logout = useUserStore.getState().logout;
   const navigate = useNavigate();
+  const { logout } = useUserStore();
 
   const handleLogoutButtonClick = () => {
     toast({
-      variant: 'destructive',
-      description: 'Are you sure you want to log out?',
+      variant: "destructive",
+      description: "Are you sure you want to log out?",
       action: (
-        <div className='flex flex-col gap-2'>
-          <ToastAction className='bg-slate-400 w-auto' altText='Confirm' onClick={handleLogOut}>Confirm</ToastAction>
-          <ToastAction className='bg-slate-400' altText='Cancel'>Cancel</ToastAction>
+        <div className="flex flex-col gap-2">
+          <ToastAction
+            className="bg-slate-400 w-auto"
+            altText="Confirm"
+            onClick={handleLogOut}
+          >
+            Confirm
+          </ToastAction>
+          <ToastAction className="bg-slate-400" altText="Cancel">
+            Cancel
+          </ToastAction>
         </div>
-      )
-    })
-  }
+      ),
+    });
+  };
 
   const handleLogOut = () => {
     logout();
-    navigate('/');
+    navigate("/");
 
     const toastId = toast({
-      description: 'You have logged out successfully.',
-    })
+      description: "You have logged out successfully.",
+    });
 
     setTimeout(() => {
       toastId.dismiss();
     }, 2000);
-  }
+  };
 
   return (
-    <Button className={className} onClick={handleLogoutButtonClick}>Logout</Button>
+    <Button className={className} onClick={handleLogoutButtonClick}>
+      Logout
+    </Button>
   );
 }
 
