6a1a7739a669a3b87c3de04a6ca27d8a53716b70
zhicong <zhicong@sunlands.com.sg>
2025-03-26 15:04:11 +0800

    update login and remove fetch user function in user store to implement backend api and update login logic in LoginForm component

--- mobile-web-shop/src/pages/login/LoginForm.jsx
+++ mobile-web-shop/src/pages/login/LoginForm.jsx
@@ -1,26 +1,22 @@
-import { Button } from '@/components/ui/button';
-import { useToast } from '@/components/hooks/use-toast';
-import { showToast } from '@/utils/toastUtils';
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/hooks/use-toast";
+import { showToast } from "@/utils/toastUtils";
 
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import useReturnPathStore from '@/store/useReturnPathStore';
-import useUserStore from '@/store/useUserStore';
+import useReturnPathStore from "@/store/useReturnPathStore";
+import useUserStore from "@/store/useUserStore";
 
 function LoginForm() {
-  const [email, setEmail] = useState('john@example.com');
-  const [password, setPassword] = useState('password');
+  const [email, setEmail] = useState("john@example.com");
+  const [password, setPassword] = useState("password");
 
   const navigate = useNavigate();
   const { toast } = useToast();
 
   const { returnPath } = useReturnPathStore();
-  const { login, fetchUser } = useUserStore();
-
-  // Temporary manually authenicate user, will be replaced with Supabase auth
-  let fetchedUser;
-  const fetchUserData = async () => await fetchUser(email);
+  const { login } = useUserStore();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -28,57 +24,64 @@ function LoginForm() {
     if (!email || !password) {
       showToast({
         toast,
-        variant: 'destructive',
-        description: 'Email and password are required.'
+        variant: "destructive",
+        description: "Email and password are required.",
       });
       return;
     }
 
-    fetchedUser = await fetchUserData();
+    const token = await login({ email, password });
+    console.log(token);
 
-    if (!fetchedUser) {
+    if (!token) {
       showToast({
         toast,
-        variant: 'destructive',
-        description: 'User not found.'
+        variant: "destructive",
+        description: "Log in failed. Please try again.",
       });
       return;
     }
 
-    if (fetchedUser.password === password) {
-      login(fetchedUser.id);
-      navigate(returnPath);
-      showToast({
-        toast,
-        description: 'You have logged in successfully.',
-        timeout: 2000
-      })
-    } else {
-      showToast({
-        toast,
-        variant: 'destructive',
-        description: 'Incorrect password.'
-      });
-    }
-  }
+    showToast({
+      toast,
+      description: "You have logged in successfully.",
+      timeout: 2000,
+    });
+    navigate(returnPath);
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label className='font-bold mb-1' htmlFor='email'>Email</label>
-      <input className='mb-5 pl-3 h-10 border border-neutral-500 rounded w-full' type='email' value={email} name='email' id='email' autoComplete='email' placeholder='me@example.com' onChange={e => setEmail(e.target.value)} />
-      <label className='font-bold mb-1' htmlFor='password'>Password</label>
+      <label className="font-bold mb-1" htmlFor="email">
+        Email
+      </label>
       <input
-        className='mb-5 pl-3 h-10 border border-neutral-500 rounded w-full'
-        type='password'
-        value={password}
-        name='password'
-        id='password'
-        placeholder='********'
-        onChange={e => setPassword(e.target.value)}
+        className="mb-5 pl-3 h-10 border border-neutral-500 rounded w-full"
+        type="email"
+        value={email}
+        name="email"
+        id="email"
+        autoComplete="email"
+        placeholder="me@example.com"
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <Button className='w-full' type='submit'>Login</Button>
+      <label className="font-bold mb-1" htmlFor="password">
+        Password
+      </label>
+      <input
+        className="mb-5 pl-3 h-10 border border-neutral-500 rounded w-full"
+        type="password"
+        value={password}
+        name="password"
+        id="password"
+        placeholder="********"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button className="w-full" type="submit">
+        Login
+      </Button>
     </form>
-  )
+  );
 }
 
 export default LoginForm;
--- mobile-web-shop/src/store/useUserStore.js
+++ mobile-web-shop/src/store/useUserStore.js
@@ -1,39 +1,39 @@
-  import { create } from 'zustand';
-  import supabase from '@/utils/supabase';
+import { create } from "zustand";
 
-  const useUserStore = create((set) => {
-    const storedUserId = JSON.parse(localStorage.getItem('userId'));
-    const loggedInUserId = storedUserId ? storedUserId : null;
+const useUserStore = create((set) => {
+  const storedUserId = JSON.parse(localStorage.getItem("userId"));
+  const loggedInUserId = storedUserId ? storedUserId : null;
 
-    return {
-      userId: loggedInUserId,
+  return {
+    userId: loggedInUserId,
 
-      login: (userId) => {
-        set({ userId });
-        localStorage.setItem('userId', JSON.stringify(userId));
-      },
+    login: async (loginDetails) => {
+      const url = "http://localhost:3000/api/user/login";
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify(loginDetails),
+        });
 
-      logout: () => {
-        set({ userId: null });
-        localStorage.removeItem('userId');
-      },
-
-      fetchUser: async (email) => {
-        try {
-          const { data: user, error } = await supabase
-            .from('user')
-            .select('*')
-            .eq('email', email)
-            .single();
-
-          if (error) throw error;
-
-          return user;
-        } catch (error) {
-          console.error('Error fetching user:', error);
+        if (!response.ok) {
+          throw new Error("Response status:", response.status);
         }
-      },
-    }
-  })
 
-  export default useUserStore;
+        const { token, userId } = await response.json();
+        set({ userId });
+        localStorage.setItem("userId", JSON.stringify(userId));
+
+        return token;
+      } catch (error) {
+        console.error("Error logging in:", error);
+      }
+    },
+
+    logout: () => {
+      set({ userId: null });
+      localStorage.removeItem("userId");
+    },
+  };
+});
+
+export default useUserStore;
